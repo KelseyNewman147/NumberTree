c4fc5b4a9d30ce27b50278224e89e443a2ac1b7b
Kelsey <kpnewman2013@gmail.com>
2018-04-19 08:01:35 -0400

    initial commit

---
+++ src/main/java/com/kelsey/NumberTree/NumberTreeApplication.java
@@ -0,0 +1,12 @@
+package com.kelsey.NumberTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberTreeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kelsey/NumberTree/NumberTreeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kelsey.NumberTree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberTreeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
