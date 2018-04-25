64661a2d020a14bfaf474d4513c30002e0c02831
Kelsey <kpnewman2013@gmail.com>
2018-04-24 21:55:23 -0400

    swing configuration in NumberTreeApplication.java

--- src/main/java/com/kelsey/NumberTree/NumberTreeApplication.java
+++ src/main/java/com/kelsey/NumberTree/NumberTreeApplication.java
@@ -2,11 +2,17 @@ package com.kelsey.NumberTree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class NumberTreeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NumberTreeApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(NumberTreeApplication.class, args);
+		JFrame frame = ctx.getBean(JFrame.class);
+		frame.setVisible(true);
+
 	}
 }
