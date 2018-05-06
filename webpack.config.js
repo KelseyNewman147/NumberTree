744e55d1340034753a5451eb5e73de18e47c1338
Kelsey <kpnewman2013@gmail.com>
2018-05-06 16:34:21 -0400

    corrected path to index.js in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/main/js/index.js',
+    entry: './src/index.js',
     cache: true,
     output: {
         path: __dirname,
