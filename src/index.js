fdbf4a94a96ceb0d5d0d0a4f27fe276c5b5d776a
Kelsey <kpnewman2013@gmail.com>
2018-05-07 01:33:23 -0400

    remove port

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class App extends React.Component{
     loadTreeFromServer() {
         var self = this;
         $.ajax({
-          url: "https://numbertreereact.herokuapp.com/api"
+          url: "numbertreereact.herokuapp.com/api"
         }).then(function (data) {
           console.log(data);
           self.setState({factories: data.factories});
