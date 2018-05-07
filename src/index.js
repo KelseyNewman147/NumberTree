79219b94b5caabba957f03a3415846ca4fc3599b
Kelsey <kpnewman2013@gmail.com>
2018-05-07 01:26:20 -0400

    added heroku url

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class App extends React.Component{
     loadTreeFromServer() {
         var self = this;
         $.ajax({
-          url: "/api"
+          url: "https://numbertreereact.herokuapp.com/api"
         }).then(function (data) {
           console.log(data);
           self.setState({factories: data.factories});
