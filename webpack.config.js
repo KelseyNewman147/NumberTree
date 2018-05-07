d6d66745fc7308b565dbfafcb17698c6fac82473
Kelsey <kpnewman2013@gmail.com>
2018-05-06 21:42:40 -0400

    removed homepage route from package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main/resources/static/index.js',
     cache: true,
     output: {
         path: __dirname,
