056c475585a8d08064ccd6eb55969fe3a155402f
Kelsey <kpnewman2013@gmail.com>
2018-06-12 21:10:37 -0400

    updated frontend-maven-plugin in pom

--- src/main/java/com/kelsey/NumberTree/services/ChildNodeRepository.java
+++ src/main/java/com/kelsey/NumberTree/services/ChildNodeRepository.java
@@ -3,7 +3,5 @@ package com.kelsey.NumberTree.services;
 import com.kelsey.NumberTree.entities.ChildNode;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface ChildNodeRepository extends CrudRepository<ChildNode, Integer> {
 }
--- src/main/java/com/kelsey/NumberTree/services/FactoryRepository.java
+++ src/main/java/com/kelsey/NumberTree/services/FactoryRepository.java
@@ -4,8 +4,6 @@ package com.kelsey.NumberTree.services;
 import com.kelsey.NumberTree.entities.Factory;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface FactoryRepository extends CrudRepository<Factory, Integer> {
     Factory findFactoryById(int id);
 }
